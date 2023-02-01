ebc3db9584e4bf66af497438e90f8da6ea210c1e
Michael Williams <michael.williams@enspiral.com>
2023-02-01 21:23:37 +1300

    bring theme into code, start tidying

--- scripts/images.js
+++ scripts/images.js
@@ -2,18 +2,22 @@
 
 'use strict';
 
-hexo.extend.filter.register('after_post_render', (data) => {
-  const image_version = hexo.extend.helper.get('image_version')
+hexo.extend.helper.register('create_img_srcset', (src) => {
+  const image_version = hexo.extend.helper.get('image_version').bind(hexo)
   const images_config = hexo.config.responsive_images
-  const post_image_config = Array.isArray(images_config) ? images_config[0] : images_config
-  const post_image_sizes = post_image_config.sizes
+  const image_config = Array.isArray(images_config) ? images_config[0] : images_config
+  const image_sizes = image_config.sizes
+  return Object.entries(image_sizes)
+    .map(([size, { width }]) => `${image_version(src, { prefix: size })} ${width}w`)
+    .join(', ')
+})
+
+hexo.extend.filter.register('after_post_render', (data) => {
+  const create_img_srcset = hexo.extend.helper.get('create_img_srcset').bind(hexo)
   data.content = data.content.replace(/<img([^>]+)?>/igm, (_, attr) => {
     attr = attr.replace(/src="([^"]+)?"/, (_, src) => {
       if (src.endsWith('svg')) return `src=${src}`
-      const srcset = Object.entries(post_image_sizes)
-        .map(([size, { width }]) => `${image_version(src, { prefix: size })} ${width}w`)
-        .join(', ')
-      return `srcset="${srcset}"`
+      return `srcset="${create_img_srcset(src)}"`
     })
     return `<img${attr}>`
   })
