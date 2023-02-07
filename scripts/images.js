365bd1b812d81955dcd7e10daff7dc6fc257084a
Michael Williams <michael.williams@enspiral.com>
2023-02-07 13:38:46 +1300

    correct img sizes, also refactor to asset dimensions to separate function

--- scripts/images.js
+++ scripts/images.js
@@ -12,24 +12,41 @@ hexo.extend.helper.register('create_responsive_img', (src, attrs) => {
   const images_config = hexo.config.responsive_images
   const image_config = Array.isArray(images_config) ? images_config[0] : images_config
   const image_sizes = image_config.sizes
+  const image_widths = Object.values(image_sizes)
 
   const img_srcset = Object.entries(image_sizes)
     .map(([size, { width }]) => `${image_version(src, { prefix: size })} ${width}w`)
     .join(', ')
+  const img_sizes = "(min-width: 992px) 992px, 100vw"
 
-  const image_sizes_length = Object.keys(image_sizes).length
-  const image_widths = Object.values(image_sizes)
-  let img_sizes = []
-  for (let i = 0; i < image_sizes_length; i++) {
-    const { width } = image_widths[i]
-    if (i === image_sizes_length - 1) {
-      img_sizes.push(`${width}px`)
-      continue
-    }
-    img_sizes.push(`(max-width: ${width}px) ${width}px`)
-  }
-  img_sizes = img_sizes.join(', ')
+  const img_attrs = typeof attrs === 'string' ?
+    attrs :
+    Object.entries(attrs).map(([key, value]) => `${key}="${value}"`).join(' ')
 
+  const { width, height } = getAssetDimensions(src, { image_widths })
+
+  return `<img srcset="${img_srcset}" sizes="${img_sizes}" src="${image_version(src, { prefix: 'lg' })}" ${img_attrs} width="${width}" height="${height}" />`
+})
+
+hexo.extend.filter.register('after_post_render', (data) => {
+  // img
+  const create_responsive_img = hexo.extend.helper.get('create_responsive_img').bind(hexo)
+  data.content = data.content.replace(/<img([^>]+)?>/igms, (_a, attrs) => {
+    const [_b, src] = attrs.match(/src="([^"]+)?"/m)
+    attrs = `${attrs} loading="lazy"`
+    const image = (!(src.endsWith('jpg') || src.endsWith('jpeg') || src.endsWith('png')))
+      ? `<img${attrs} />`
+      : create_responsive_img(src, attrs)
+    return `<div class="image-wrapper">${image}</div>`
+  })
+
+  // video
+  data.content = data.content.replace(/<video([^>]+)?>(.*?)<\/video>/igms, (_, attrs, children) => {
+    return `<div class="video-wrapper"><video${attrs}>${children}</video></div>`
+  })
+})
+
+function getAssetDimensions(src, { image_widths }) {
   const PostAsset = hexo.model('PostAsset')
   let asset = PostAsset.findById(join('source/_posts', src))
   if (asset == null) {
@@ -64,31 +81,9 @@ hexo.extend.helper.register('create_responsive_img', (src, attrs) => {
       throw new Error(`Unexpected JPEG orientation: ${dimensions.orientation}`)
   }
   const max_image_width = Math.max(...image_widths.map(({ width }) => width))
-  const max_width = Math.min(max_image_width, dimensions.width)
-  const ratio = max_width / dimensions.width
-  const max_height = (dimensions.height * ratio).toFixed(0)
+  const width = Math.min(max_image_width, dimensions.width)
+  const ratio = width / dimensions.width
+  const height = (dimensions.height * ratio).toFixed(0)
 
-  const img_attrs = typeof attrs === 'string' ?
-    attrs :
-    Object.entries(attrs).map(([key, value]) => `${key}="${value}"`).join(' ')
-
-  return `<img srcset="${img_srcset}" sizes="${img_sizes}" src="${image_version(src, { prefix: 'lg' })}" ${img_attrs} width="${max_width}" height="${max_height}" />`
-})
-
-hexo.extend.filter.register('after_post_render', (data) => {
-  // img
-  const create_responsive_img = hexo.extend.helper.get('create_responsive_img').bind(hexo)
-  data.content = data.content.replace(/<img([^>]+)?>/igms, (_a, attrs) => {
-    const [_b, src] = attrs.match(/src="([^"]+)?"/m)
-    attrs = `${attrs} loading="lazy"`
-    const image = (!(src.endsWith('jpg') || src.endsWith('jpeg') || src.endsWith('png')))
-      ? `<img${attrs} />`
-      : create_responsive_img(src, attrs)
-    return `<div class="image-wrapper">${image}</div>`
-  })
-
-  // video
-  data.content = data.content.replace(/<video([^>]+)?>(.*?)<\/video>/igms, (_, attrs, children) => {
-    return `<div class="video-wrapper"><video${attrs}>${children}</video></div>`
-  })
-})
+  return { width, height }
+}
