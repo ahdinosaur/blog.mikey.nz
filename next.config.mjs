7daea216a8abfe40c6242bd5ae7822351d009837
Michael Williams <hello@mikey.nz>
2026-05-11 17:01:06 +1200

    vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
-  async redirects() {
-    return [
-      {
-        source: '/gifts-of-a-charitable-interpretation/',
-        destination: '/being-charitable/',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 export default nextConfig
